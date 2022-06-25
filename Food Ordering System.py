7426ac37738100d85993100b463bd6bc560b763a
Gan Aik Tong <andrewganaiktong@gmail.com>
2022-06-25 17:33:43 +0800

    Updated

--- Food Ordering System.py
+++ Food Ordering System.py
@@ -1,23 +1,23 @@
-import turtle
-from Modules import main_menu
+import turtle                                   #import for turtle
+from Modules import main_menu                   #import from main menu
 
 ########################
 ###  WELCOME WINDOW  ###
 ########################
 
-wn = turtle.Screen()
-wn.bgcolor("light blue")
-wn.title("Food Ordering System - Welcome Page")
-tess = turtle.Turtle()
+wn = turtle.Screen()                            #turtle drawing during the opening of the program
+wn.bgcolor("light blue")                        #set the window background colour
+wn.title("Food Ordering System - Welcome Page") #set the window title
+tess = turtle.Turtle()                          #create tess and set some attributes
 tess.hideturtle()
-tess.penup()
-tess.color('black')
+tess.penup()                                    #is used to pick up the pen and turtle stop drawing
+tess.color('black')                             #is used to give black colour on the star
 tess.goto(-75, 200)
-tess.pendown()
-tess.speed(10)
-for i in [0, 1, 2, 3, 4]:
-    tess.forward(100)
-    tess.right(144)
+tess.pendown()                                  #starts to draw star pattern
+tess.speed(10)                                  #speed setting can be set between 1(slowest) to 10(fastest)
+for i in [0, 1, 2, 3, 4]:                       #executing loop 5 times for a star
+    tess.forward(100)                           #moving turtle 100 units forward
+    tess.right(144)                             #rotating turtle 144 degree right
 
 tess.penup()
 tess.goto(-180, 90)
@@ -26,11 +26,11 @@ style1 = ('Courier', 30, 'bold')
 tess.write('W E L C O M E', font=style1)
 
 tess.penup()
-tess.goto(-300, 10)
+tess.goto(-320, 10)
 tess.pendown()
 
 style2 = ('Courier', 20, 'italic')
-tess.write("\nYou are using Food Ordering System", font=style2)
+tess.write("\nYou are using UTEM Food Ordering System", font=style2) # entering the welcome page of food ordering system
 
 tess.penup()
 tess.goto(100, -50)
@@ -38,7 +38,7 @@ tess.pendown()
 
 tess.speed(7)
 
-tess.color('brown')
+tess.color('brown')                               #is used to give brown colour on the word "Click to continue"
 style3 = ('Courier', 10, 'italic')
 tess.write("Click to continue...", font=style3)
 
@@ -57,5 +57,6 @@ wn.exitonclick()
 #######################
 
 main_menu()
+
 
 
